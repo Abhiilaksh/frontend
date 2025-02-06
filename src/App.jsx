5c2d1b4e69f9d0e967e2b66d0c36c15b07b2db89
Arpan <145697806+arpanmehta05@users.noreply.github.com>
2025-02-06 16:35:59 +0530

    feat: add landing page with video background and routing, implement chessboard component

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,22 @@
+import React from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Full from "./landing/Full";
 import PlayWithEngine from "./engine/PlayWithEngine";
 
 function App() {
-
   return (
-    <div
-    className='
-        bg-[#121212]
-        h-[100vh]
-        w-[100vw]
-        text-white
-    '
-    >
-      <h1>Chess App</h1>
-      <PlayWithEngine />
-    </div>
-  )
+    <BrowserRouter>
+      <div
+        className='bg-[#121212] h-[100vh] w-[100vw] text-white'
+      >
+        <Routes>
+          <Route path="/" element={<Full />} />
+          <Route path="/chess" element={<PlayWithEngine />} />
+          <Route path="*" element={<div>Not Found</div>} />
+        </Routes>
+      </div>
+    </BrowserRouter>
+  );
 }
 
-export default App
+export default App;
