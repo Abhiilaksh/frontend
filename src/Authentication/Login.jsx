14c81e8d57a9a3b4667c454494c2b1ac502cd73e
Aiyaret Sandhu <arshsandhuprofessional@gmail.com>
2025-03-04 17:18:24 +0530

    feat : changes

--- src/Authentication/Login.jsx
+++ src/Authentication/Login.jsx
@@ -4,8 +4,17 @@ import { GoogleOAuthProvider } from '@react-oauth/google';
 
 export default function Login() {
   return (
-    <GoogleOAuthProvider clientId={`${import.meta.env.VITE_REACT_APP_GOOGLE_CLIENT_ID}`}>
-      <Template type={"login"} text1={"Don't have an account?"} text2={"Sign Up"} heading={"Login to"} />
+    <GoogleOAuthProvider 
+      clientId={`${import.meta.env.VITE_REACT_APP_GOOGLE_CLIENT_ID}`}
+    >
+
+      <Template 
+        type={"login"} 
+        text1={"Don't have an account?"} 
+        text2={"Sign Up"} 
+        heading={"Login to"} 
+      />
+      
     </GoogleOAuthProvider>
   )
 }
--- src/Authentication/Signup.jsx
+++ src/Authentication/Signup.jsx
@@ -4,8 +4,16 @@ import { GoogleOAuthProvider } from "@react-oauth/google";
 
 export default function Signup() {
   return (
-    <GoogleOAuthProvider clientId={`${import.meta.env.VITE_REACT_APP_GOOGLE_CLIENT_ID}`}>
-      <Template type={"signup"} text1={"Already have an account?"} text2={"Login"} heading={"Signup For"}/>
+    <GoogleOAuthProvider 
+      clientId={`${import.meta.env.VITE_REACT_APP_GOOGLE_CLIENT_ID}`}>
+
+      <Template 
+        type={"signup"} 
+        text1={"Already have an account?"} 
+        text2={"Login"} 
+        heading={"Signup For"}
+      />
+    
     </GoogleOAuthProvider>
   );
 }
